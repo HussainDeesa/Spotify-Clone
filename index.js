e207931ad0873b435ff7e95c4ebcc8cc4c715541
Hussain <hussainmurtaza.cse_b2019@crescent.education>
2022-03-20 18:01:02 +0530

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const connectToMongo = require('./db')
 const app = express()
-const port = 7000
+const port = process.env.PORT||7000
 
 connectToMongo();
 app.use(express.json())
